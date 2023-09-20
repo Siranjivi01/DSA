d91f5ac0ae2e1211883b614956b7021dd76e09ac
githiyoun55 <githiyounn@gmail.com>
2023-09-20 11:00:22 +0530

    Altered

--- DSA - 1/Stack.py
+++ DSA - 1/Stack.py
@@ -1,23 +1,24 @@
 def create_stack():
      stack=[]
      return stack
+def check_stack(stack):
+    if len(stack)==0:
+        print("Stack is Empty")
 
 def push_stack(stack,item):
     stack.append(item)
     print("Show Pushed Items",item)
     return stack
-def check_stack():
-    if len(stack)=0:
-        print("Stack is Empty")
-
-def pop_stack():
+def pop_stack(stack):
     if check_stack(stack)==0:
         print("Stack is Empty, So Wont Remove")
-    return stack.pop()
+    stack.pop()
+    print(stack)
+    return stack
 
 stackarr=create_stack()
-check_stack()
+check_stack(stackarr)
 push_stack(stackarr,"Hello")
 push_stack(stackarr,"Hello World")
 push_stack(stackarr,"Stack Code")
-pop_stack()
+pop_stack(stackarr)
