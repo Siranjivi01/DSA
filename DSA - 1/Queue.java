860a01d71e50e0e7d2289942acc42614fcf7c2aa
githiyoun55 <githiyounn@gmail.com>
2023-09-26 14:21:08 +0530

    Circular Queue added in Queue.java

--- DSA - 1/Queue.java
+++ DSA - 1/Queue.java
@@ -1,4 +1,73 @@
-class Main{
+class FormalQueue{
+    private int size=10;
+    private int Front,Rear;
+    private int arr[]=new int[size];
+    FormalQueue(){
+        Front=-1;
+        Rear=-1;
+    }
+    public boolean CheckEmpty(){
+        if(Front==-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean CheckFull(){
+        if(Front==0 && Rear==size-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public void Enqueue(int x){
+        if(CheckFull()){
+            System.out.println("Queue has Already Full");
+            System.exit(1);
+        }
+        else{
+            Front=0;
+            Rear=0;
+            arr[++Rear]=x;
+            System.out.println("Enqueue Value" +x);
+        }
+    }
+    public void Dequeue(){
+        int element;
+        if(CheckEmpty()){
+            System.out.println("Queue has Already Empty");
+            System.exit(1);
+        }
+        else{
+            element=arr[Front];
+            if(Front==Rear){
+                Front=-1;
+                Rear=-1;
+            }
+            System.out.println("Dequeue Item:"+element);
+        }
+    }
+    public void DisplayQueue(){
+        for(int i=Front;i<=Rear;i++){
+            System.out.print(arr[i]);
+        }
+    }
+    public static void main(String[] args)
+{
+        FormalQueue q=new FormalQueue();
+        q.Enqueue(10);
+        q.Enqueue(20);
+        q.Enqueue(30);
+        q.Enqueue(40);
+        q.DisplayQueue();
+}
+}
+
+/* Circular Queue
+
+ * class Main{
     private int Front,Rear;
     private int Size=10;
     private int data[]=new int[Size];
@@ -63,13 +132,14 @@ public void DisplayQueue(){
         System.out.println("Queue Values: "+data[i]);
     }
 }
-
 public static void main(String[] args){
 Main s=new Main();
 s.Enqueue(20);
 s.Enqueue(30);
 s.Enqueue(50);
+s.Enqueue(80);
 s.Dequeue();
 s.DisplayQueue();
 }
 }
+*/
